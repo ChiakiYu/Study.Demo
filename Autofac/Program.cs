6ad521cf85e8238c4d21fe52e4c62645f936fa13
醉丶千秋 <yu_199012@qq.com>
2015-06-27 16:29:52 +0800

    学习IOC，DI

---
+++ Autofac/Program.cs
@@ -0,0 +1,27 @@
+﻿using Autofac.Application;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DatabaseManager>();
+            builder.RegisterType<SqlDatabase>().As<IDatabase>();
+            using (var container = builder.Build())
+            {
+                var manager = container.Resolve<DatabaseManager>();
+                manager.Search("SELECT * FORM USER");
+            }
+            Console.ReadKey();
+        }
+    }
+}
