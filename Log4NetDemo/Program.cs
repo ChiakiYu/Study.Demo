41e3c59bba3256d793a75b95cf1df860ccc9970a
醉丶千秋 <yu_199012@qq.com>
2015-07-02 12:47:28 +0800

    demo

---
+++ Log4NetDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogData();
+        }
+
+        public static void LogData()
+        {
+            //记录一般信息
+            LogHelper.Info("info");
+            //记录调试信息
+            LogHelper.Debug("debug");
+            //记录警告信息
+            LogHelper.Warn("warn");
+
+            //记录错误日志
+            LogHelper.Error("error", new Exception("发生了一个异常"));
+            //记录严重错误
+            LogHelper.Fatal("fatal", new Exception("发生了一个致命错误"));
+
+            Console.WriteLine("日志记录完毕。");
+            Console.Read();
+
+
+        }
+    }
+}
