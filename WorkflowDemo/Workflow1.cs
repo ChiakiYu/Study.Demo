4ec7b40996ddeeda38783a412a7735531861406e
醉丶千秋 <yu_199012@qq.com>
2015-07-04 17:46:02 +0800

    工作流

---
+++ WorkflowDemo/Workflow1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Linq;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+
+namespace WorkflowDemo
+{
+    public sealed partial class Workflow1 : SequentialWorkflowActivity
+    {
+        private string _str;
+
+        public string Str
+        {
+            get { return _str; }
+            set { _str = value; }
+        }
+
+
+        public Workflow1()
+        {
+            InitializeComponent();
+        }
+
+        private void EvaluatePostalCode(object sender, ConditionalEventArgs e)
+        {
+            e.Result = _str.Equals("1");
+        }
+
+        private void PostalCodeVaild(object sender, EventArgs e)
+        {
+            Console.Write("{0}：验证通过", _str);
+            Console.ReadKey();
+        }
+
+        private void PostalCodeInVaild(object sender, EventArgs e)
+        {
+            Console.Write("{0}：没通过验证", _str);
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ WorkflowDemo/Workflow1.designer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Reflection;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+
+namespace WorkflowDemo
+{
+    partial class Workflow1
+    {
+        #region Designer generated code
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("", "")]
+        private void InitializeComponent()
+        {
+            this.CanModifyActivities = true;
+            System.Workflow.Activities.CodeCondition codecondition1 = new System.Workflow.Activities.CodeCondition();
+            this.codeActivity2 = new System.Workflow.Activities.CodeActivity();
+            this.codeActivity1 = new System.Workflow.Activities.CodeActivity();
+            this.ifElseBranchActivity2 = new System.Workflow.Activities.IfElseBranchActivity();
+            this.ifElseBranchActivity1 = new System.Workflow.Activities.IfElseBranchActivity();
+            this.ifElseActivity1 = new System.Workflow.Activities.IfElseActivity();
+            // 
+            // codeActivity2
+            // 
+            this.codeActivity2.Name = "codeActivity2";
+            this.codeActivity2.ExecuteCode += new System.EventHandler(this.PostalCodeInVaild);
+            // 
+            // codeActivity1
+            // 
+            this.codeActivity1.Name = "codeActivity1";
+            this.codeActivity1.ExecuteCode += new System.EventHandler(this.PostalCodeVaild);
+            // 
+            // ifElseBranchActivity2
+            // 
+            this.ifElseBranchActivity2.Activities.Add(this.codeActivity2);
+            this.ifElseBranchActivity2.Name = "ifElseBranchActivity2";
+            // 
+            // ifElseBranchActivity1
+            // 
+            this.ifElseBranchActivity1.Activities.Add(this.codeActivity1);
+            codecondition1.Condition += new System.EventHandler<System.Workflow.Activities.ConditionalEventArgs>(this.EvaluatePostalCode);
+            this.ifElseBranchActivity1.Condition = codecondition1;
+            this.ifElseBranchActivity1.Name = "ifElseBranchActivity1";
+            // 
+            // ifElseActivity1
+            // 
+            this.ifElseActivity1.Activities.Add(this.ifElseBranchActivity1);
+            this.ifElseActivity1.Activities.Add(this.ifElseBranchActivity2);
+            this.ifElseActivity1.Name = "ifElseActivity1";
+            // 
+            // Workflow1
+            // 
+            this.Activities.Add(this.ifElseActivity1);
+            this.Name = "Workflow1";
+            this.CanModifyActivities = false;
+
+        }
+
+        #endregion
+
+        private IfElseBranchActivity ifElseBranchActivity2;
+
+        private IfElseBranchActivity ifElseBranchActivity1;
+
+        private CodeActivity codeActivity1;
+
+        private CodeActivity codeActivity2;
+
+        private IfElseActivity ifElseActivity1;
+
+
+
+
+    }
+}
