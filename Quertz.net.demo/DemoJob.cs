11631c402b2caefab569d154b89e9c43d16bbb76
醉丶千秋 <yu_199012@qq.com>
2015-12-23 09:12:57 +0800

    Quertz.net.demo

---
+++ Quertz.net.demo/DemoJob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Quartz;
+
+namespace Quertz.net.demo
+{
+    public class DemoJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("作业执行!"+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+}
---
+++ Quertz.net.demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace Quertz.net.demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+            scheduler.Start(); //开启调度器
+
+            var job = JobBuilder.Create<DemoJob>()
+                .WithIdentity("myJob", "group1")
+                .UsingJobData("jobSays", "Hello World!")
+                .Build();
+
+
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("mytrigger", "group1")
+                .StartNow()
+                .WithCronSchedule("/5 * * ? * *") //时间表达式，5秒一次     
+                .Build();
+
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
